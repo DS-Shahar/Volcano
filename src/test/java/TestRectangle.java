5f5570f7243a177e6fa87c2742a1f0c8ea348c86
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2022-07-19 20:58:51 +0300

    Update TestRectangle.java

--- src/test/java/TestRectangle.java
+++ src/test/java/TestRectangle.java
@@ -6,17 +6,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class TestRectangle {
+public class TestVolcano {
 
    @Test
-   public void testProps()
+   public void testConstructor()
    {
-     Rectangle r = new Rectangle(10, 20);
-
-     int w = r.getWidth();
-     assertEquals(w, 10);
-     
-     int h = r.getHeight();
-     assertEquals(h, 20);
+      Volcano v1 = new Volcano("Etna", 450);
+      int c = v1.getCount();
+      assertEquals(c, 0);
+      String n = v1.getName();
+      assertEquals(n, "Etna");
    }
+   
+   @Test
+   public void testMethods()
+   {
+      Volcano v1 = new Volcano("Etna", 450);
+      v1.add(1400);
+      v1.add(1300);
+      v1.add(1200);
+      assertEquals(v1.getCount(), 3);
+      v1.fix(1, 1350);
+      v1.delete(1);
+      assertEquals(v1.getCount(), 2);
+      double a = v1.average();
+      assertEquals(a, 1300);
+      int m = v1.maxUnder(1300);
+      assertEquals(m, 1);
+   }
+   
 }
