d98fa5ebe231d0813187754531e3e6ad962e67d4
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2022-07-19 21:38:02 +0300

    Update TestVolcano.java

--- src/test/java/TestVolcano.java
+++ src/test/java/TestVolcano.java
@@ -8,34 +8,64 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestVolcano {
 
-   @Test
-   public void testConstructor()
-   {
-      Volcano v1 = new Volcano("Etna", 450);
-      int c = v1.getCount();
-      assertEquals(c, 0);
-      String n = v1.getName();
-      assertEquals(n, "Etna");
+   private Volcano v1;
+   
+   @BeforeAll
+   public void setup() {
+      v1 = new Volcano("Etna", 12);
+   }
+   
+   @BeforeEach     
+   public void init() {
    }
    
    @Test
-   public void testMethods()
+   @DisplayName("Test simple constructor")
+   public void testConstructor()
    {
-      Volcano v1 = new Volcano("Etna", 450);
+      Volcano v = new Volcano("Etna", 45);
+      int c = v.getCount();
+      assertEquals(c, 0);
+      String n = v.getName();
+      assertEquals(n, "Etna");
+   }  
+
+   @Test
+   public void testAdd()
+   {
       v1.add(1400);
       v1.add(1300);
       v1.add(1200);
       assertEquals(v1.getCount(), 3);
+   }
+   @Test
+   public void testFix() {
+      testAdd();
       v1.fix(1, 1350);
+   }
+   @Test
+   public void testDelete() {
+      testAdd();
       v1.delete(1);
       assertEquals(v1.getCount(), 2);
+   }
+   @Test
+   public void testAverage() {
+      testAdd();
       double a = v1.average();
       assertEquals(a, 1300);
-      int m = v1.maxUnder(1300);
+   }
+   @Test
+   public void testMaxUnder() {
+      testAdd();
+      int m = v1.maxUnder(1400);
       assertEquals(m, 1);
+   }
+   @Test
+   public void testCopyConstructor() {
+      testAdd();
       Volcano v2 = new Volcano(v1);
       assertEquals(v2.getName(), "Etna");
-      assertEquals(v2.getCount(), 2);
-   }
-   
+      assertEquals(v2.getCount(), 3);
+   }  
 }
